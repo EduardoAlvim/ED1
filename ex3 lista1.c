f274bf53d6de4a6252877a7d2cfd3ba4d65ef93a
EduardoAlvim <105020093+EduardoAlvim@users.noreply.github.com>
2022-05-17 14:09:22 -0300

    Update ex3 lista1.c

--- ex3 lista1.c
+++ ex3 lista1.c
@@ -10,8 +10,8 @@ void main()
     float custof, custo, pdd, imp;
     printf("\nDigite o custo de fabrica do carro: ");
     scanf("%f",&custof);
-    pdd = custof*(28/100);
-    imp = custof*(45/100);
+    pdd = custof*0.28;
+    imp = custof*0.45;
     custo = custof + pdd + imp;
     printf("\nO custo de seu carro sera: %.2f",custo);
 }
